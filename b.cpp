4937b49420122ffbbd41ea2c46daa865abb216ec
maxLundin <lundin.v.20202020@gmail.com>
2018-10-11 22:10:57 +0300

    b

--- b.cpp
+++ b.cpp
@@ -4,11 +4,37 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-vector<uint32_t> mas[100000];
+vector<pair<uint32_t, uint32_t>> mas[200001];
+int used[200001];
+int timein[200001];
+int up[200001];
+vector<uint32_t> ans;
+uint32_t time1 = 0;
 
+void dfs(int n, int predok = -1) {
+    used[n] = 1;
+    timein[n] = ++time1;
+    up[n] = time1;
+    for (int i = 0; i < mas[n].size(); i++) {
+        if(mas[n][i].first == predok){
+            continue;
+        }
+        if (used[mas[n][i].first]) {
+            up[n] = min(up[n], timein[mas[n][i].first]);
+        } else {
+           // predok = n;
+            dfs(mas[n][i].first, n);
+            up[n] = min(up[n], up[mas[n][i].first]);
+            if (up[mas[n][i].first] > timein[n]) {
+                ans.push_back(mas[n][i].second);
+            }
+        }
+    }
+}
 
 int main() {
     int n, m;
@@ -18,10 +44,21 @@ int main() {
     uint32_t a1, a2;
     for (int i = 0; i < m; i++) {
         cin >> a1 >> a2;
-        mas[a1].push_back(a2);
+        mas[a1].emplace_back(a2, i + 1);
+        mas[a2].emplace_back(a1, i + 1);
     }
 
+    for (int i = 1; i <= n; i++) {
+        if (used[i] != 1) {
+            dfs(i);
+        }
+    }
 
+    cout << ans.size() << "\n";
+    sort(ans.begin(),ans.end());
+    for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << " ";
+    }
 
     return 0;
 }
