867bfe82c06eb1ad84f44f84b4b8de312337c9fd
maxLundin <lundin.v.20202020@gmail.com>
2018-11-03 13:09:21 +0300

    diskra

--- B1Diskra.cpp
+++ B1Diskra.cpp
@@ -2,3 +2,62 @@
 // Created by max on 02.11.2018.
 //
 
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool mas[4000][4000];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    freopen("chvatal.in", "r", stdin);
+    freopen("chvatal.out", "w", stdout);
+
+    int n;
+    cin >> n;
+
+    char c;
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            cin >> c;
+            mas[i][j] = (c == '1');
+            mas[j][i] = mas[i][j];
+        }
+    }
+
+
+    deque<int> queue;
+    for (int i = 0; i < n; i++) {
+        queue.push_back(i);
+    }
+    label:
+    for (int i = 0; i < 14; i++) {
+        random_shuffle(queue.begin(), queue.end());
+    }
+    int i = 0;
+    for (int k = 0; k < n * (n - 1); k++) {
+        if (!mas[queue[0]][queue[1]]) {
+            i = 2;
+            while (i < n && (!mas[queue[0]][queue[i]] || !mas[queue[1]][queue[i + 1]])) {
+                i++;
+            }
+            if (i == n) {
+                goto label;
+            }
+
+//            std::cout << "i: "<< i <<std::endl;
+            reverse(queue.begin() + 1, queue.begin() + i + 1);
+//            random_shuffle(queue.begin()+1, queue.begin()+i);
+        }
+        queue.push_back(queue[0]);
+//        queue.erase(queue.begin());
+        queue.pop_front();
+    }
+
+    for (int i : queue) {
+        cout << i + 1 << " ";
+    }
+
+}
+
